ab9ac699b15830f057894fcf1f5e0d3f2f33ad3c
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2020-02-05 21:43:43 -0500

    finish

--- src/navi/navi.jsx
+++ src/navi/navi.jsx
@@ -8,8 +8,8 @@ import HamburgerLogo from '../hamburger/hamburgerLogo/hamburgerLogo'
 
 function Navi() {
 
-  useEffect(()=> {
-    gsap.from(".navi-container-2", {autoAlpha:0,y:-30,duration:1.5, delay:0.5, ease:"power2"})
+  useEffect(() => {
+    gsap.from(".navi-container-2", { autoAlpha: 0, y: -30, duration: 1.5, delay: 0.5, ease: "power2" })
   })
 
   useScrollPosition(({ prevPos, currPos }) => {
@@ -22,60 +22,60 @@ function Navi() {
     <div className='navi-container'>
       <div className="navi-container-2">
 
-          <div className="logo">JWP <span>Web Developer</span></div>
+        <div className="logo">JWP <span>Web Developer</span></div>
 
         <div className="navi-spacer"></div>
 
         <div className="navi-links">
 
-        <div className="navi-link-container">
-          <Link
-            className='link'
-            activeClass="active"
-            to="home"
-            spy={true}
-            smooth={true}
-            duration={1200} >
-            Home
+          <div className="navi-link-container">
+            <Link
+              className='link'
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={1200} >
+              Home
           </Link>
           </div>
           <div className="navi-link-container">
-          <Link
-            className='link'
-            activeClass="active"
-            to="projects"
-            spy={true}
-            smooth={true}
-            duration={1200} >
-            Projects
+            <Link
+              className='link'
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              duration={1200} >
+              Projects
           </Link>
           </div>
           <div className="navi-link-container">
-          <Link
-            className='link'
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            duration={1200} >
-            About
+            <Link
+              className='link'
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={1200} >
+              About
           </Link>
           </div>
           <div className="navi-link-container">
-          <Link
-            className='link'
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            duration={1200} >
-            Contact
+            <Link
+              className='link'
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              duration={1200} >
+              Contact
           </Link>
           </div>
         </div>
       </div>
       <div className="hamburger-logo-wrapper">
-      <HamburgerLogo />
+        <HamburgerLogo />
       </div>
     </div>
   )
